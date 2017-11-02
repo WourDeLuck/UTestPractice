a00eb01c500b3362faa4f4536fa1a811a6d99a84
WourDeLuck <thesakurawolf@gmail.com>
2017-11-02 14:47:19 +0300

    Initial commit

---
+++ UTestPractice.Tests/MethodTests.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UTestPractice.Tests
+{
+    [TestFixture]
+    public class MethodTests
+    {
+        [TestCase]
+        public void Search_For_The_Maximum_Value_Recursively_Test()
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+            int[] array = { 5, 2, 0, 3, 19, 10, 34, 2, 1 };
+
+            var res = alg.RecursiveSearch(array, 0, 0);
+
+            Assert.AreEqual(34, res);
+        }
+
+        [TestCase]
+        public void Concatenate_Sort_And_Delete_Duplicates_From_String_Test()
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+            string str1 = "hello";
+            string str2 = "you";
+
+            var answer = alg.ConcatenateString(str1, str2);
+
+            Assert.AreEqual("ehlouy", answer);
+        }
+
+        [TestCase("mine", "not")]
+        [TestCase("greatest", "weapon")]
+        [TestCase("Helo", "Ehlo")]
+        public void More_Tests_For_String_Concatenating(string s1, string s2)
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+
+            var answer = alg.ConcatenateString(s1, s2);
+
+            Assert.IsNotEmpty(answer);
+        }
+
+        [TestCase]
+        public void Wrong_Characters_Input_For_Concatenating_Method_Test()
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+            string str1 = "Hello world!";
+            string str2 = "You done?";
+
+            var answer = alg.ConcatenateString(str1, str2);
+
+            Assert.IsNull(answer);
+        }
+
+        [TestCase]
+        public void Leave_Lucky_Numbers_Test()
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+            List<int> list = new List<int>();
+            list.Add(9);
+            list.Add(17);
+            list.Add(57);
+            list.Add(3);
+            list.Add(10);
+            list.Add(70);
+
+            alg.FilterLucky(list);
+
+            Assert.IsTrue(list.Count == 3);
+        }
+
+        [TestCase]
+        public void Summing_Up_Left_And_Right_Elements_Of_An_Array_Test()
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+            int[] array = { 2, 3, 1, 1 };
+
+            var answer = alg.ArraySumSplat(array);
+
+            Assert.AreEqual(1, answer);
+        }
+
+        [TestCase]
+        public void Null_Input_For_Summing_Up_Test()
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+            int[] array = { 4, 5, 6, 1, 0 };
+
+            var answer = alg.ArraySumSplat(array);
+
+            Assert.AreEqual(0, answer);
+        }
+
+        [TestCase(5, 3, 1, 2, ExpectedResult = 7)]
+        [TestCase(102, 23, 0, 4, ExpectedResult = 119)]
+        [TestCase(1024, 19, 2, 6, ExpectedResult = 1100)]
+        public int Insert_M_Into_N_Testing(int n, int m, int i, int j)
+        {
+            DifferentAlgorhytms alg = new DifferentAlgorhytms();
+
+            var answer = alg.BitsAndIntegers(n, m, i, j);
+
+            return answer;
+        }
+    }
+}
---
+++ UTestPractice/DifferentAlgorhytms.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UTestPractice
+{
+    /// <summary>
+    /// Class contains different algorithms to test with NUnit.
+    /// </summary>
+    public class DifferentAlgorhytms
+    {
+        /// <summary>
+        /// Inserts one number inside of other based on the position given.
+        /// </summary>
+        /// <param name="num1">Number 1.</param>
+        /// <param name="num2">Number 2 smaller than the first one.</param>
+        /// <param name="i">Begining of the position.</param>
+        /// <param name="j">End of the position.</param>
+        /// <returns></returns>
+        public int BitsAndIntegers(int num1, int num2, int i, int j)
+        {
+            int maskOfOnes = ~0;
+
+            int left = maskOfOnes << (j + 1);
+
+            int right = ((1 << i) - 1);
+
+            int mask = left | right;
+
+            int num1_cleared = num1 & mask;
+            int num2_shifted = num2 << i;
+
+            return (num1_cleared | num2_shifted);
+        }
+
+        /// <summary>
+        /// Recursive search algorithm. Returns maximum element in the array.
+        /// </summary>
+        /// <param name="array">Array to search in.</param>
+        /// <param name="index">The index to start with.</param>
+        /// <param name="max">Current maximum element.</param>
+        /// <returns></returns>
+        public int RecursiveSearch(int[] array, int index, int max)
+        {
+            if (index < array.Length -1)
+            {
+                if (array[index] > max)
+                    max = array[index];
+
+                return RecursiveSearch(array, index + 1, max);
+            }
+            return max;
+        }
+
+        /// <summary>
+        /// Returns an element's index if the sums from both sides are equal.
+        /// Returns 0 if there is no such element.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public int ArraySumSplat(int[] array)
+        {
+            // To count the sum of elements on the right.
+            int rightSum = 0;
+            // To count the sum of elements on the left.
+            int leftSum = 0;
+            // The answer (as index).
+            int ourAnswer = 0;
+
+            for (int i = 1; i<array.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    leftSum = leftSum + array[j];
+                for (int j = array.Length - 1; j > i; j--)
+                    rightSum = rightSum + array[j];
+
+                if (leftSum == rightSum)
+                    ourAnswer = i;
+                else continue;
+            }
+            return ourAnswer;           
+        }
+
+        /// <summary>
+        /// Algorithm for concatenating strings.
+        /// Also removes duplicates and sorts.
+        /// Other characters than letters are not allowed.
+        /// </summary>
+        /// <param name="str1">First string to work with.</param>
+        /// <param name="str2">Second string to work with.</param>
+        /// <returns></returns>
+        public string ConcatenateString(string str1, string str2)
+        {
+            if (Regex.IsMatch(str1, @"^[a-zA-Z]+$") && Regex.IsMatch(str2, @"^[a-zA-Z]+$"))
+            {
+                string str3 = str1 + str2;
+                char[] a = str3.ToCharArray();
+                Array.Sort(a);
+                return new string(a.Distinct().ToArray());
+            }
+            else return null;
+        }
+
+        /// <summary>
+        /// Removes any numbers in the list of integers that have no 7 in their value.
+        /// </summary>
+        /// <param name="list">List to operate with.</param>
+        public void FilterLucky(List<int> list)
+        {
+            for (int i=0;i<list.Count;i++)
+            {
+                if (!i.ToString().Contains(7.ToString()))
+                    list.RemoveAt(i);
+            }
+        }
+    }
+}
